3577610b22b495a9bcb8256a1169314238515ba2
Mentus99 <gabriel.mendes.rodrigues@gmail.com>
2025-10-28 10:37:33 -0300

    Correções

--- js/modal.js
+++ js/modal.js
@@ -1,22 +1,32 @@
 let overlay = null;
 let modal = null;
 let closeBtn = null;
+let registerLink = null; // <- Variável nova
 
-// 1. Pega os elementos do HTML e prepara o botão de fechar
+// 1. Pega os elementos do HTML e prepara os botões
 export function initModal() {
     overlay = document.getElementById('project-modal-overlay');
     modal = document.getElementById('project-modal');
     closeBtn = document.getElementById('modal-close-btn');
+    
+    // Pega o botão de cadastro de dentro do modal
+    registerLink = document.getElementById('modal-register-link');
+    // --- FIM DA LINHA NOVA ---
 
-    if (overlay && closeBtn) {
+    if (overlay && closeBtn && registerLink) {
         // Fecha o modal se clicar no 'X'
         closeBtn.addEventListener('click', closeModal);
+        
         // Fecha o modal se clicar fora dele (no overlay)
         overlay.addEventListener('click', (e) => {
             if (e.target === overlay) {
                 closeModal();
             }
         });
+
+        // Fecha o modal se clicar no link "Quero Participar"
+        registerLink.addEventListener('click', closeModal);
+        // --- FIM DO BLOCO NOVO ---
     }
 }
 
@@ -31,6 +41,7 @@ export function openModal(project) {
     document.getElementById('modal-badge').textContent = project.badge;
     document.getElementById('modal-description').textContent = project.description;
     
+    // Atualiza o link do botão (se for o caso)
     // Mostra o modal
     overlay.classList.add('active');
 }
